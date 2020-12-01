6146260beafa62404ec822b7063fa26b5dec7ddf
Anurag1810 <anuragup95@gmail.com>
2020-11-30 16:27:52 -0800

    Uploading the project.

---
+++ static/js/page-translations.js
@@ -0,0 +1,61 @@
+var linguJSON = {
+    "languages": [
+        {
+            "lang_name": "English",
+            "lang_code": "en",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "Español",
+            "lang_code": "es",
+            "url_pattern": "?"
+        }
+    ],
+    "translated_segments": [
+        {
+            "source": "Login",
+            "target": "Iniciar sesión"
+        },
+        {
+            "source": "Home",
+            "target": "Hogar",
+        },
+        {
+            "source": "Guest Registration",
+            "target": "Registro de invitado",
+        },
+        {
+            "source": "All Events",
+            "target": "Todos los Eventos",
+        },
+        {
+            "source": "Seminar",
+            "target": "Seminario",
+        },
+        {
+            "source": "Course",
+            "target": "Curso",
+        },
+        {
+            "source": "Password",
+            "target": "Contraseña",
+        },
+        {
+            "source": "Course",
+            "target": "Curso",
+        },
+        {
+            "source": "Course",
+            "target": "Curso",
+        },
+        {
+            "source": "Course",
+            "target": "Curso",
+        },
+        {
+            "source": "Course",
+            "target": "Curso",
+        }
+        
+    ]
+};
