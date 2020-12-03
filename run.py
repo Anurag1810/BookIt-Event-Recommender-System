97db931a693fa95604404441fa64c676d6c32a34
Anurag1810 <anuragup95@gmail.com>
2020-12-03 15:46:37 -0800

    Added UI fixes and added profiling.

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 from app import app
 if __name__ == '__main__':
-    app.run(debug=True, port=9999)
+    import cProfile, pstats
+
+    profiler = cProfile.Profile()
+    profiler.enable()
+    app.run(debug=True)
+    profiler.disable()
+    stats = pstats.Stats(profiler).sort_stats('ncalls')
+    stats.print_stats()
+    stats.dump_stats('stats_file.dat')
