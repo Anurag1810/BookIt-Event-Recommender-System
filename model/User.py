1f080db55c1bbf8ae4db250017f10ad9968f57ce
Anurag1810 <anuragup95@gmail.com>
2020-12-03 16:58:45 -0800

    Fixed minor db issues.

--- model/User.py
+++ model/User.py
@@ -4,7 +4,7 @@ from flask_mail import Message
 import secrets
 
 class User(UserMixin, db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+	id = db.Column(db.String(80), primary_key=True)
 	name = db.Column(db.String(80), nullable=False)
 	password = db.Column(db.String(80), nullable=False)
 	email = db.Column(db.String(80), nullable=False)
